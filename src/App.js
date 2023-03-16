eb3cba6eb5f96a301c0c72d13cdb20641cb8c605
Afaq-302 <ufaq302@yahoo.com>
2023-03-16 16:41:41 +0500

    Added Loader

--- src/App.js
+++ src/App.js
@@ -2,35 +2,46 @@ import React, { useState, useEffect } from 'react';
 import ImageCard from './components/ImageCard';
 import ImageSearch from './components/ImageSearch';
 import './assets/tailwind.css';
+import MoonLoader from "react-spinners/MoonLoader";
+
 
 function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [term,setTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
 
   useEffect(() => {
-    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${searchTerm}&orientation=horizontal`)
       .then(response => response.json())
       .then(data => {
         setImages(data.hits);
-        setIsLoading(false);
+
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 2000)
+
       })
       .catch(err => console.log(err))
-  }, [term])
+  }, [searchTerm]);
+
+
 
   return (
     <div className='container mx-auto'>
-      <ImageSearch searchText={(text) => setTerm(text)} />
+      <ImageSearch searchText={(text) => setSearchTerm(text)} />
 
-    {!isLoading && images.length === 0 && <h1 className='text-5xl mx-auto text-center mt-32'>No Images Found!</h1>}
+      {!isLoading && images.length === 0 && <h1 className='text-5xl mx-auto text-center mt-32'>No Images Found!</h1>}
 
-      {isLoading ? <h1 className='text-5xl mx-auto text-center mt-32'>Loading...</h1> : <div className='grid grid-cols-3 gap-4'>
-        {
-          images.map(image => (
-            <ImageCard key={image.id} image={image} />
-          ))
-        }
-      </div>}
+      {isLoading ?
+        <div className='flex justify-center items-center mt-32'><MoonLoader color="#36d7b7" /></div>
+        :
+        <div className='grid grid-cols-3 gap-4'>
+          {
+            images.map(image => (
+              <ImageCard key={image.id} image={image} />
+            ))
+          }
+        </div>}
     </div>
 
   );
